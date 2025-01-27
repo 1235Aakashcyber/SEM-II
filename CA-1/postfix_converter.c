e6e668bd9a0af8cb221428506a20420ca2e88146
Aakash Goswami <183493923+1235Aakashcyber@users.noreply.github.com>
2025-01-27 21:24:15 +0530

    Update postfix_converter.c

--- CA-1/postfix_converter.c
+++ CA-1/postfix_converter.c
@@ -1,75 +1,72 @@
-#include<stdio.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <ctype.h>
 #define max 20
-int top=-1;
+
+int top = -1;
 char stack[max];
+
 void push(char value);
-int pop();
+char pop();
 int priority(char input);
-int main(){
-char input[50];
-char out;
-printf("Enter the equation : ");
-fgets(input,50,stdin);
-for(int i=0; input[i]!='\0'; i++){
-    if(isalnum(input[i])){
-        printf("%c",input[i]);
-    }
-    else if(input[i]=='('){
-        push(input[i]);
-    }
-    else if(input[i]==')'){
-        while((out = pop()) != '('){
-            printf("%c",out);
+char isoperator(char c);
+
+int main() {
+    char input[50];
+    char out;
+    printf("Enter the equation: ");
+    fgets(input, 50, stdin);
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (isalnum(input[i])) {
+            printf("%c", input[i]);
+        } else if (input[i] == '(') {
+            push(input[i]);
+        } else if (input[i] == ')') {
+            while ((out = pop()) != '(') {
+                printf("%c", out);
+            }
+        } else if (isoperator(input[i])) {
+            while (top != -1 && priority(stack[top]) >= priority(input[i])) {
+                printf("%c", pop());
+            }
+            push(input[i]);
         }
     }
-    // else if(input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/' || input[i]=='%'){
-    //     push(input[i]);
-    // }
-    else if(priority(stack[top])>=priority(input[i]) || top==-1){
-            printf("%c",pop());
-            push(input[i]);
-        
+    while (top != -1) {
+        printf("%c", pop());
     }
-}
-while (top!=-1)
-{
-    printf("%c",pop());
+    return 0;
 }
 
-return 0;
+void push(char value) {
+    if (top == (max - 1)) {
+        printf("Stack overflow\n");
+        return;
+    }
+    top++;
+    stack[top] = value;
 }
 
-void push(char value){
-if(top==(max-1)){
-return;
-}
-else{
-top++;
-stack[top] = value;
-}
+char pop() {
+    if (top == -1) {
+        printf("Stack underflow\n");
+        return '\0';
+    }
+    return stack[top--];
 }
 
-int pop(){
-char ret;
-if(top == -1){
-return 0;
-}
-else{
-ret = stack[top];
-top--;
-return ret;
-}
-}
-
-int priority(char input){
-    // if(input=='('){
-    //     return 0;
-    // }
-    if(input=='+' || input=='-'){
+int priority(char input) {
+    if (input == '(' || input == ')') {
+        return 0;
+    }
+    if (input == '+' || input == '-') {
         return 1;
     }
-    else if(input == '*' || input == '/' || input=='%'){
+    if (input == '*' || input == '/' || input == '%') {
         return 2;
     }
+    return -1; // For invalid characters
+}
+
+char isoperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
 }
